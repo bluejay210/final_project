39a420e9fef10d7b503bd0c8067b704e0fb91aec
Yunghwan <syhmanse@gmail.com>
2016-10-16 02:23:01 -0400

    heroku ready

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from googlefinance import getQuotes
 app = Flask(__name__)
@@ -35,4 +36,5 @@ def results():
 		message = "Nothing Found"
 		return render_template('results.html', search_result=media, message=message)
 
-app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
